3e6871aac85f1d07a78d736aac827086233605a0
faisalibal33 <faisaliqbal030397@gmail.com>
2022-02-22 10:09:28 +0700

    change hooks

---
+++ src/component/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+  return (
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+      <div>
+        <h2 className='grow br3 shadow-5'>{name}</h2>
+        <h4>{email}</h4>
+      </div>
+    </div>
+  );
+}
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Faisal Herroix',
+      username: 'faisal',
+      skill: 'Musician'
+    },
+    {
+      id: 2,
+      name: 'Milos',
+      username: 'inyos',
+      skill: 'Big Cat'
+    },
+    {
+      id: 3,
+      name: 'Hiro',
+      username: 'ilo',
+      skill: 'Milos Son'
+    },
+    {
+      id: 4,
+      name: 'Faisal Iqbal',
+      username: 'ibal',
+      skill: 'FullStack Developer'
+    },
+    {
+      id: 5,
+      name: 'Febiona',
+      username: 'ona',
+      skill: 'Faisal Girlfriend'
+    },
+    {
+      id: 6,
+      name: 'Restvet',
+      username: 'Restvet',
+      skill: 'Wizard'
+    },
+    {
+      id: 7,
+      name: 'Imperial',
+      username: 'imperial',
+      skill: 'Fighter'
+    },
+    {
+      id: 8,
+      name: 'Rotor',
+      username: 'thor',
+      skill: 'Wind'
+    },
+    {
+      id: 9,
+      name: 'Sayuki',
+      username: 'yuki',
+      skill: 'Prince'
+    },
+    {
+      id: 10,
+      name: 'Masamura',
+      username: 'mura',
+      skill: 'Dark Magic'
+    }
+  ];
+
